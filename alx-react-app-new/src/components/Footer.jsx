22b9211b8965dbb86d88c0ef9d73c319cdb8fb47
fekreM <fekreselassie.mulu@gmail.com>
2025-09-07 00:28:18 +0300

    week 10 task 0

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        backgroundColor: "#f5f5f5",
+        padding: "16px",
+        textAlign: "center",
+      }}
+    >
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+export default Footer;
---
+++ alx-react-app-new/src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header
+      style={{ backgroundColor: "navy", color: "white", textAlign: "center" }}
+    >
+      <h1>My Favorite Cities</h1>
+    </header>
+  );
+}
+export default Header;
