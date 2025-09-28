8c6d76979ea8bc3ced6057c888697ea3cfb9188d
Fekreselassie <fekruson@gmail.com>
2025-09-28 16:40:25 +0300

    task 3

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const UserProfile = () => {
   return (
-    <div className="mx-auto sm:p-4 md:p-8 max-w-xs md:max-w-sm bg-white rounded-lg shadow-md flex flex-col items-center">
+    <div className="mx-auto sm:p-4 md:p-8 max-w-xs md:max-w-sm bg-white rounded-lg shadow-md flex flex-col items-center transition-shadow duration-300 ease-in-out hover:shadow-xl">
       <img
         src="https://randomuser.me/api/portraits/men/75.jpg"
         alt="User Profile"
-        className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 object-cover mb-4"
+        className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 object-cover mb-4 transition-transform duration-300 ease-in-out hover:scale-110"
       />
-      <h2 className="font-bold text-lg md:text-xl mb-2 text-gray-800">
+      <h2 className="font-bold text-lg md:text-xl mb-2 text-gray-800 transition-colors duration-300 ease-in-out hover:text-blue-500">
         John Doe
       </h2>
       <p className="text-sm md:text-base text-gray-600 text-center">
